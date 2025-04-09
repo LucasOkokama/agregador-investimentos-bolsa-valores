18dff0cb24748ef4b5fe01308f320b1cc27fbe63
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-09 12:01:32 -0300

    feat: add UserController endpoints

---
+++ src/main/java/tech/buildrun/agregadorinvestimentos/controller/CreateUserDTO.java
@@ -0,0 +1,4 @@
+package tech.buildrun.agregadorinvestimentos.controller;
+
+public record CreateUserDTO(String username, String email, String password) {
+}
---
+++ src/main/java/tech/buildrun/agregadorinvestimentos/controller/UserController.java
@@ -0,0 +1,30 @@
+package tech.buildrun.agregadorinvestimentos.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.buildrun.agregadorinvestimentos.entity.User;
+import tech.buildrun.agregadorinvestimentos.service.UserService;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+  private UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @PostMapping
+  public ResponseEntity<User> createUser(@RequestBody CreateUserDTO createUserDTO){
+    var userId = userService.createUser(createUserDTO);
+    return ResponseEntity.created(URI.create("/v1/users/" + userId.toString())).build();
+  }
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<User> getUserById(@PathVariable("userId") String userId){
+    return null;
+  }
+}
