5ca8e942e974d748f11c13ddcff7a6cf031563b0
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-10 15:48:44 -0300

    feat: add repositories for multiple entities

--- src/main/java/tech/buildrun/agregadorinvestimentos/repository/BillingAddressRepository.java
+++ src/main/java/tech/buildrun/agregadorinvestimentos/repository/BillingAddressRepository.java
@@ -1,9 +1,11 @@
 package tech.buildrun.agregadorinvestimentos.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tech.buildrun.agregadorinvestimentos.entity.BillingAddress;
 
 import java.util.UUID;
 
+@Repository
 public interface BillingAddressRepository extends JpaRepository<BillingAddress, UUID> {
 }
